de55b90d2db98075528ec26a125c8681dbc16eec
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-03-30 02:25:35 +0530

    update astro config for static file deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://safeack.github.io',
+  base: 'safeack.github.io',
 });
